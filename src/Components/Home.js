039b0bb34c9021174793c26ee8bdeeddcbfcba38
Sindhuja Vangipuram <vj27021995@gmail.com>
2023-05-20 13:41:00 +0530

    fixed the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import photo from '../photo.jpg'
 import { Image } from 'semantic-ui-react'
 import { Paper, MenuList, MenuItem,ListItemText,Divider,Box} from '@mui/material'
@@ -10,7 +10,9 @@ import Education from './Education'
 import Achievement from './Achievement'
 import Poc from './Poc'
 export default function Home(props) {
-
+  const [minutes,setMinutes] = useState(1)
+  const [timeLeft, setTimeLeft] = useState( minutes* 60);
+ 
   const [displayState, setDisplayState] = useState({
     Skills: false,
     WorkExperience: false,
@@ -22,8 +24,34 @@ export default function Home(props) {
 
   const handleMenuClick = (key) => {
     setDisplayState({ ...Object.fromEntries(Object.entries(displayState).map(([k, v]) => [k, k === key])) });
+    setTimeLeft(minutes* 60);
   };
   const { Skills, WorkExperience, Certifications, Educations, Achievements, POC } = displayState;
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+
+    const displayInterval = setInterval(() => {
+      setDisplayState({
+        Skills: false,
+        WorkExperience: false,
+        Certifications: false,
+        Educations: false,
+        Achievements: false,
+        POC: false,
+      });
+    }, timeLeft*1000);
+
+    return () => {
+      clearInterval(timeInterval);
+      clearInterval(displayInterval);
+    };
+  }, [timeLeft]);
+
+const minutesLeft = Math.floor(timeLeft / 60);
+const secondsLeft = timeLeft % 60;
 
   return (
     <div>
@@ -34,11 +62,18 @@ export default function Home(props) {
         '& > :not(style)': {
           m: 1,
         },
-        justifyContent:'center',
-        marginLeft:'18%'
       }}
     >
+      {(Skills || WorkExperience || Certifications || Educations || Achievements ||POC) && <div  style={{flex:1,justifyContent:'flex-start',alignItems:'flex-start'}}><p>Redirecting to Home page in {minutesLeft}:{secondsLeft < 10 ? "0" : ""}{secondsLeft}</p></div>}
+      <div  style={{flex:1,justifyContent:'center',marginLeft:(Skills || WorkExperience || Certifications || Educations || Achievements ||POC)?'8.6%':'19.6%'}}>
+    <div style={{display:'flex',alignItems:(Skills || WorkExperience || Certifications || Educations || Achievements ||POC)?'':'center',justifyContent:(Skills || WorkExperience || Certifications || Educations || Achievements ||POC)?'':'center'}}>
     <Image src={photo} size='small' rounded />
+   
+    {(Skills || WorkExperience || Certifications || Educations || Achievements ||POC) && (<p style={{ fontSize: '200%', fontWeight: 'bold',padding:'4% 0% 0% 0%',margin:10 }}>
+           Vijay Kumar
+          </p>)}
+          </div>
+          </div>
     </Box>
      
       <Box
@@ -67,6 +102,7 @@ export default function Home(props) {
                   onClick={() => handleMenuClick(key)}
                 >
                   <ListItemText
+                    //className={Object.values(displayState).includes(true) && !displayState[key] ? '' : key}
                     className={key}
                     style={{ color: displayState[key] ? 'white' : 'black' }}
                   >
@@ -87,7 +123,7 @@ export default function Home(props) {
       {!Object.values(displayState).some((v) => v) && (
         <div>
           <p style={{ display: 'flex', justifyContent: 'center', fontSize: '200%', fontWeight: 'bold' }}>
-            Mr. Vijay Kumar
+           Vijay Kumar
           </p>
           <p style={{ display: 'flex', justifyContent: 'center', fontSize: '200%', fontWeight: 'bold' }}>
           Front End (React JS) Developer
